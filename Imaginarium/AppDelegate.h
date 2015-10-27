03530dacdb4f3f426d305c7832f42c92a9cac4e2
Admin <admin@Mac-Admin.local>
2015-10-27 15:21:43 +0200

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Admin on 27.10.15.
+//  Copyright (c) 2015 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
